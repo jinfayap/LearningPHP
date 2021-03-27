d3e1dbfe2eb4a7370d672640cdefa1c58f6e3471
jinfayap <jinfayap@gmail.com>
2021-03-27 16:41:12 +0800

    Lesson 4

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
-<?php
+<html lang="en">
 
-// variable $
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    header {
+        background: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+    }
+    </style>
+</head>
 
-$greeting = 'Hello';
-$name = 'Jin fa';
+<body>
+    <header>
 
-echo $greeting. ' '. $name;
+        <h1>
+        <?= "Hello, ". htmlspecialchars($_GET['name']);?>
+        </h1>
+    </header>
+</body>
+
+</html>
