dc2c820e2e460ef3ef7764c80e38c005fbcd40df
jinfayap <jinfayap@gmail.com>
2021-03-28 12:53:19 +0800

    Dry Up Your Views

--- views/about-culture.view.php
+++ views/about-culture.view.php
@@ -1,11 +1,5 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php require('partials/head.php') ?>
+
     <h1>Our Culture at <?= $name ?></h1>
-</body>
-</html>
+    
+<?php require('partials/footer.php') ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,23 +1,5 @@
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <nav>
-        <ul>
-            <li>
-                <a href="/about.php">About</a>
-            </li>
-            <li>
-                <a href="/contact.php">Contact</a>
-            </li>
-        </ul>
-    </nav>
+<?php require('partials/head.php')?>
+<h1>My Tasks</h1>
     <ul>
         <?php foreach($tasks as $task) : ?>
             <li>
@@ -31,6 +13,4 @@
         <?php endforeach; ?>
     </ul>
 
-</body>
-
-</html>
+<?php require('partials/footer.php') ?>
