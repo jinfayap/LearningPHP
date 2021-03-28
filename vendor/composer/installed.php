260cab5ca04cd97ce5ec7fcd380a6a8fdcfc2ce9
jinfayap <jinfayap@gmail.com>
2021-03-28 14:37:37 +0800

    Composer Autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1ff04224a52dbdcaf9c567c080b744d3d22bf6d1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1ff04224a52dbdcaf9c567c080b744d3d22bf6d1',
+    ),
+  ),
+);
